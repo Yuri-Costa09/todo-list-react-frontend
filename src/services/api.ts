0d433d185b2fc55d8af0a5fc87477fc784d0923b
yuri-costa-flooxai <yurircostawork@gmail.com>
2025-05-20 14:10:17 -0300

    refactor: domain name change

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios, { type InternalAxiosRequestConfig } from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://3.137.177.29/api',
+    baseURL: 'https://aitrip.one/api',
     headers: {
         'Content-Type': 'application/json',
     },
